8642f4fb2f4f1d41fd463b4d88120a5b095bfde3
Jens A. Koch <jakoch@web.de>
2024-07-20 21:26:44 +0200

    Update SetupVcpkg.cmake, VCPKG_ROOT is defined on CI.. VCPKG_TOOLCHAIN_FILE should be set automatically

--- cmake/SetupVcpkg.cmake
+++ cmake/SetupVcpkg.cmake
@@ -1,11 +1,7 @@
 #
 # SetupVcpkg
 #
-# This configures vcpkg using environment variables instead of a command-line options.
-#
-# https://github.com/microsoft/vcpkg/blob/master/docs/users/integration.md#using-an-environment-variable-instead-of-a-command-line-option
-#
-# Environment Variables: https://vcpkg.readthedocs.io/en/latest/users/config-environment/
+# This CMake script configures vcpkg using env variables instead of CLI options.
 #
 
 set(VCPKG_VERBOSE ON)
@@ -14,13 +10,12 @@ if(DEFINED ENV{VCPKG_VERBOSE} AND NOT DEFINED VCPKG_VERBOSE)
     set(VCPKG_VERBOSE "$ENV{VCPKG_VERBOSE}" CACHE BOOL "")
 endif()
 
-#
-# -- Automatic install of vcpkg dependencies.
-#
-# This is experimental.
-# See https://github.com/Microsoft/vcpkg/issues/1653
-#
+# Use aria2 for faster downloads. By default curl is used.
+set(VCPKG_INSTALL_OPTIONS "--x-use-aria2")
 
+#
+# Automatic installation of vcpkg dependencies.
+#
 set(CMAKE_POLICY_DEFAULT_CMP0077 NEW)
 set(X_VCPKG_APPLOCAL_DEPS_INSTALL ON)
 set(VCPKG_APPLOCAL_DEPS_INSTALL ON)
@@ -32,28 +27,29 @@ endif()
 
 # Copy dependencies into the install target directory for executables.
 if(DEFINED ENV{X_VCPKG_APPLOCAL_DEPS_INSTALL} AND NOT DEFINED VCPKG_APPLOCAL_DEPS)
-
     # X_VCPKG_APPLOCAL_DEPS_INSTALL depends on CMake policy CMP0087
     if(POLICY CMP0087)
         cmake_policy(SET CMP0087 NEW)
     endif()
-
     set(X_VCPKG_APPLOCAL_DEPS_INSTALL "$ENV{X_VCPKG_APPLOCAL_DEPS_INSTALL}" CACHE BOOL "")
 endif()
 
 #
-# --  VCPKG_ROOT
+# VCPKG_ROOT
 #
 # Please set VCPKG_ROOT on your env: export VCPKG_ROOT=/opt/vcpkg/bin
 # This avoids passing it on the configure line: -DCMAKE_TOOLCHAIN_FILE=C:\vcpkg\scripts\buildsystems\vcpkg.cmake
 #
-if (DEFINED CMAKE_TOOLCHAIN_FILE)
+# If VCPKG_ROOT is not set, but the toolchain, we set root using the CMAKE_TOOLCHAIN_FILE.
+# If CMAKE_TOOLCHAIN_FILE is not set, but VCPKG_ROOT, we set the toolchain using the VCPKG_ROOT.
+#
+if(DEFINED CMAKE_TOOLCHAIN_FILE)
     if (NOT DEFINED ENV{VCPKG_ROOT})
         string(REPLACE "/scripts/buildsystems/vcpkg.cmake" "" _VCPKG_ROOT "${CMAKE_TOOLCHAIN_FILE}")
         set(ENV{VCPKG_ROOT} "${_VCPKG_ROOT}")
     endif()
 elseif (DEFINED ENV{VCPKG_ROOT})
-    if (NOT DEFINED CMAKE_TOOLCHAIN_FILE)
+    if(NOT DEFINED CMAKE_TOOLCHAIN_FILE)
         set(CMAKE_TOOLCHAIN_FILE "$ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake" CACHE STRING "")
     endif()
 else()
@@ -61,7 +57,7 @@ else()
 endif()
 
 #
-# -- VCPKG_FEATURE_FLAGS
+# VCPKG_FEATURE_FLAGS
 #
 # This env var can be set to a comma-separated list of off-by-default features in vcpkg.
 #
@@ -78,7 +74,7 @@ if(NOT DEFINED VCPKG_FEATURE_FLAGS)
 endif()
 
 #
-# -- VCPKG_TARGET_TRIPLET
+# VCPKG_TARGET_TRIPLET
 #
 # A triplet defines the build target environment in a compact string.
 # [target-architecture]-[platform]-[linkage type]
@@ -97,7 +93,7 @@ if(DEFINED ENV{VCPKG_TARGET_TRIPLET} AND NOT DEFINED VCPKG_TARGET_TRIPLET)
 endif()
 
 #
-# -- VCPKG_DEFAULT_TRIPLET
+# VCPKG_DEFAULT_TRIPLET
 #
 if(DEFINED ENV{VCPKG_DEFAULT_TRIPLET} AND NOT DEFINED VCPKG_TARGET_TRIPLET)
   set(VCPKG_TARGET_TRIPLET "$ENV{VCPKG_DEFAULT_TRIPLET}" CACHE STRING "")
@@ -123,7 +119,7 @@ if (DEFINED VCPKG_TARGET_TRIPLET AND "${VCPKG_TARGET_TRIPLET}" MATCHES ".*-stati
     endif()
 else()
     if (NOT BUILD_SHARED_LIBS)
-        #message(FATAL_ERROR "When the VCPKG_TARGET_TRIPLET does not end with '-static' the BUILD_SHARED_LIBS must be 'ON'.")
+        # if VCPKG_TARGET_TRIPLET does not end with '-static' then set BUILD_SHARED_LIBS to 'ON' by default
         set(BUILD_SHARED_LIBS ON)
     endif()
 endif()
@@ -138,8 +134,10 @@ message(STATUS "")
 message(STATUS "[INFO]   ENV.VCPKG_ROOT                -> '$ENV{VCPKG_ROOT}'")
 message(STATUS "[INFO]   BUILD_SHARED_LIBS             -> '${BUILD_SHARED_LIBS}'")
 message(STATUS "[INFO]   CMAKE_TOOLCHAIN_FILE          -> '${CMAKE_TOOLCHAIN_FILE}'")
+
 message(STATUS "")
 message(STATUS "[VCPKG]  VCPKG_VERBOSE                 -> '${VCPKG_VERBOSE}'")
+message(STATUS "[VCPKG]  VCPKG_INSTALL_OPTIONS         -> '${VCPKG_INSTALL_OPTIONS}'")
 message(STATUS "[VCPKG]  VCPKG_APPLOCAL_DEPS           -> '${VCPKG_APPLOCAL_DEPS}'")
 message(STATUS "[VCPKG]  VCPKG_FEATURE_FLAGS           -> '${VCPKG_FEATURE_FLAGS}'")
 message(STATUS "[VCPKG]  VCPKG_MANIFEST_FILE           -> '${VCPKG_MANIFEST_FILE}'")
