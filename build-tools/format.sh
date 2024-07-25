1935b22cb59ae66312f222ccfe04ab3e5139df45
Jens A. Koch <jakoch@web.de>
2024-07-25 12:46:33 +0200

    added build-tools folder with cpplint, format, tidy

---
+++ build-tools/format.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: 2024 Jens A. Koch.
+# This file is part of https://github.com/jakoch/wikifolio_universe_converter.
+
+# Allow ENV.CLANG_FORMAT to define the path to the binary or default to clang-format
+CLANG_FORMAT=${CLANG_FORMAT:-clang-format}
+
+# Check clang-format version
+VERSION=$("$CLANG_FORMAT" --version)
+
+# Require clang-format 17 or 18 for consistent formatting features
+if [[ ! $VERSION =~ "version 17" && ! $VERSION =~ "version 18" ]]; then
+    echo "Error: Unsupported clang-format version. Must be version 17 or 18."
+    echo "Found version: $VERSION"
+    exit 1
+fi
+
+# Scan the top-level directory and subdirectories for .h and .cpp files
+
+# first convert line endings to Unix format
+# this step is skipped in CI environments
+if [[ -z "$CI" && -z "$GITHUB_ACTION" ]]; then
+    find . -type f \( -name "*.hpp" -o -name "*.cpp" \) -exec dos2unix {} \;
+fi
+
+# then apply clang-format
+find . -type f \( -name "*.hpp" -o -name "*.cpp" \) -print0 | xargs -0 $CLANG_FORMAT -i -style=file
+
+# In the CI context, we run `git diff --exit-code`.
+# After clang-format finishes, we check for changes with `git diff`.
+# If there are changes, we exit with a non-zero status code, causing the CI job to fail.
+# This ensures that code formatting is enforced.
+if [[ -n "$CI" || -n "$GITHUB_ACTION" ]]; then
+    if ! git diff --exit-code; then
+        echo "Error: Code formatting issues detected. Please run ./build-tools/format.sh and commit the changes."
+        exit 1
+    fi
+fi
---
+++ build-tools/tidy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: 2024 Jens A. Koch.
+# This file is part of https://github.com/jakoch/wikifolio_universe_converter.
+
+# Get the directory of the script
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Set the path to the compile_commands.json file
+COMPILE_COMMANDS="${SCRIPT_DIR}/../out/compile_commands.json"
+
+# Check if the compile_commands.json file exists
+if [ ! -f "$COMPILE_COMMANDS" ]; then
+    echo "Please run cmake to generate compile_commands.json before running this script."
+    exit 1
+fi
+
+# Change directory to the top-level directory
+cd "${SCRIPT_DIR}/../" || exit
+
+# This runs clang-tidy on all source files.
+clang-tidy -p "${COMPILE_COMMANDS}" src/*.hpp src/*.cpp
