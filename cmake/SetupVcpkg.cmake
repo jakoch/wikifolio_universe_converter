1d599693201aed15bd75204b96942533b1b173ac
Jens A. Koch <jakoch@web.de>
2025-08-31 14:18:25 +0200

    CMakeLists and SetupVcpkg cleanup

--- cmake/SetupVcpkg.cmake
+++ cmake/SetupVcpkg.cmake
@@ -10,48 +10,41 @@ if(DEFINED ENV{VCPKG_VERBOSE} AND NOT DEFINED VCPKG_VERBOSE)
     set(VCPKG_VERBOSE "$ENV{VCPKG_VERBOSE}" CACHE BOOL "")
 endif()
 
-# Use aria2 for faster downloads. By default curl is used.
-set(VCPKG_INSTALL_OPTIONS "--x-use-aria2")
-
 #
 # Automatic installation of vcpkg dependencies.
+# Still experimental?
 #
 set(CMAKE_POLICY_DEFAULT_CMP0077 NEW)
 set(X_VCPKG_APPLOCAL_DEPS_INSTALL ON)
-set(VCPKG_APPLOCAL_DEPS_INSTALL ON)
 
 # Copy dependencies into the output directory for executables.
-if(DEFINED ENV{VCPKG_APPLOCAL_DEPS} AND NOT DEFINED VCPKG_APPLOCAL_DEPS)
-    set(VCPKG_APPLOCAL_DEPS "$ENV{VCPKG_APPLOCAL_DEPS}" CACHE BOOL "")
-endif()
+# if(DEFINED ENV{VCPKG_APPLOCAL_DEPS} AND NOT DEFINED VCPKG_APPLOCAL_DEPS)
+#     set(VCPKG_APPLOCAL_DEPS "$ENV{VCPKG_APPLOCAL_DEPS}" CACHE BOOL "")
+# endif()
 
-# Copy dependencies into the install target directory for executables.
-if(DEFINED ENV{X_VCPKG_APPLOCAL_DEPS_INSTALL} AND NOT DEFINED VCPKG_APPLOCAL_DEPS)
-    # X_VCPKG_APPLOCAL_DEPS_INSTALL depends on CMake policy CMP0087
-    if(POLICY CMP0087)
-        cmake_policy(SET CMP0087 NEW)
-    endif()
-    set(X_VCPKG_APPLOCAL_DEPS_INSTALL "$ENV{X_VCPKG_APPLOCAL_DEPS_INSTALL}" CACHE BOOL "")
-endif()
+# # Copy dependencies into the install target directory for executables.
+# if(DEFINED ENV{X_VCPKG_APPLOCAL_DEPS_INSTALL} AND NOT DEFINED VCPKG_APPLOCAL_DEPS)
+#     # X_VCPKG_APPLOCAL_DEPS_INSTALL depends on CMake policy CMP0087
+#     if(POLICY CMP0087)
+#         cmake_policy(SET CMP0087 NEW)
+#     endif()
+#     set(X_VCPKG_APPLOCAL_DEPS_INSTALL "$ENV{X_VCPKG_APPLOCAL_DEPS_INSTALL}" CACHE BOOL "")
+# endif()
 
 #
 # VCPKG_ROOT
 #
 # Please set VCPKG_ROOT on your env: export VCPKG_ROOT=/opt/vcpkg/bin
-# This avoids passing it on the configure line: -DCMAKE_TOOLCHAIN_FILE=C:\vcpkg\scripts\buildsystems\vcpkg.cmake
 #
-# If VCPKG_ROOT is not set, but the toolchain, we set root using the CMAKE_TOOLCHAIN_FILE.
-# If CMAKE_TOOLCHAIN_FILE is not set, but VCPKG_ROOT, we set the toolchain using the VCPKG_ROOT.
-#
+# If CMAKE_TOOLCHAIN_FILE is:
+# - is set, we don't change it. This is the default case!
+# - not set, but VCPKG_ROOT, we set the toolchain using the VCPKG_ROOT,
+#   This avoids passing -DCMAKE_TOOLCHAIN_FILE. This is way shorter!
+
 if(DEFINED CMAKE_TOOLCHAIN_FILE)
-    if (NOT DEFINED ENV{VCPKG_ROOT})
-        string(REPLACE "/scripts/buildsystems/vcpkg.cmake" "" _VCPKG_ROOT "${CMAKE_TOOLCHAIN_FILE}")
-        set(ENV{VCPKG_ROOT} "${_VCPKG_ROOT}")
-    endif()
-elseif (DEFINED ENV{VCPKG_ROOT})
-    if(NOT DEFINED CMAKE_TOOLCHAIN_FILE)
-        set(CMAKE_TOOLCHAIN_FILE "$ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake" CACHE STRING "")
-    endif()
+    # do nothing, CMAKE_TOOLCHAIN_FILE is already set
+elseif(DEFINED ENV{VCPKG_ROOT} AND NOT DEFINED CMAKE_TOOLCHAIN_FILE)
+    set(CMAKE_TOOLCHAIN_FILE "$ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake" CACHE STRING "")
 else()
     message(FATAL_ERROR "One of -DCMAKE_TOOLCHAIN_FILE or the VCPKG_ROOT environment variable must be set.")
 endif()
@@ -61,16 +54,8 @@ endif()
 #
 # This env var can be set to a comma-separated list of off-by-default features in vcpkg.
 #
-# Available features are: manifests, binarycaching.
-#
-# manifests     -> use the project-local manifest file "vcpkg.json" to build dependencies
-# binarycaching -> use prebuild packages from cache to avoid rebuilds
-#
-# https://vcpkg.readthedocs.io/en/latest/specifications/manifests/
-# https://vcpkg.readthedocs.io/en/latest/specifications/binarycaching/
-#
 if(NOT DEFINED VCPKG_FEATURE_FLAGS)
-    set(VCPKG_FEATURE_FLAGS "manifests")
+    set(VCPKG_FEATURE_FLAGS "manifests,versions,binarycaching,registries")
 endif()
 
 #
@@ -103,8 +88,7 @@ iF(NOT DEFINED VCPKG_MANIFEST_FILE)
     set(VCPKG_MANIFEST_FILE "${CMAKE_SOURCE_DIR}/vcpkg.json")
 endif()
 
-# Add this file and the VCPKG_MANIFEST_FILE as a "vcpkg" source_group to the IDE.
-# They are not automatically picked up and listed as "important project" files by IDEs, yet.
+# Define an additional source group for IDEs with vcpkg relevant files.
 source_group("vcpkg" FILES
     "${CMAKE_SOURCE_DIR}/cmake/SetupVcpkg.cmake"
     "${CMAKE_SOURCE_DIR}/vcpkg.json"
